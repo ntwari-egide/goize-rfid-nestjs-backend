c3e00dea181a506270a2181df08ad0e7499f1181
yombi <ntwariegide2@gmail.com>
2021-05-05 22:29:12 +0200

    working with transaction

---
+++ src/interfaces/transaction.interface.ts
@@ -0,0 +1,8 @@
+import { Document } from "mongoose";
+
+export default interface TransactionModel extends Document{
+    card_uuid: String,
+    initialBalance: Number
+    transportFare: Number,
+    Date: Date
+}
