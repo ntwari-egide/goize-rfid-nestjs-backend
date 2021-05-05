a17d97e06c1e90f02632822eb91fe76c63dba7f4
yombi <ntwariegide2@gmail.com>
2021-05-05 22:50:43 +0200

    implementing transaction controller

--- src/controllers/transaction.controllers.ts
+++ src/controllers/transaction.controllers.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post, Req } from '@nestjs/common'
 import CardModel from 'src/interfaces/card.interface';
 import TransactionModel from 'src/interfaces/transaction.interface';
+import { CardServices } from 'src/services/cards.services';
 import { TransactionServices } from 'src/services/transaction.services';
 
 
@@ -8,7 +9,7 @@ import { TransactionServices } from 'src/services/transaction.services';
 export default class TransactionController{
     listOfDoctors : object[]
 
-    constructor(private transactionServices: TransactionServices){
+    constructor(private transactionServices: TransactionServices,private cardServices: CardServices){
     }
 
     @Get()
@@ -18,6 +19,7 @@ export default class TransactionController{
 
     @Post()
     async addDoctor(@Body() transaction: TransactionModel){
+        const card =  this.cardServices.findByUUID(transaction.card_uuid)
         this.transactionServices.create(transaction)
     }
 }
