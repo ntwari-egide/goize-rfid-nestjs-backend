5db98a672ef0e154b08c165ea9c1b2afcb1c54e6
yombi <ntwariegide2@gmail.com>
2021-05-06 06:06:01 +0200

    adding transaction module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { CardsModule } from './module/card.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TransactionModule } from './module/transaction.module';
 
 @Module({
-  imports: [CardsModule],
+  imports: [CardsModule,TransactionModule],
   controllers: [AppController],
   providers: [AppService],
 })
